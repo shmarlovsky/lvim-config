3627ffa0eaaf70cbeed58c84185201ce7e883894
Kiryl Shmarlouski <kiryl.shmarlouski@ext.fmc-ag.com>
2023-07-05 09:15:47 +0300

    prettier autoformat for css files

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@
  `lvim` is the global options object
 ]]
 -- vim options
-vim.opt.wrap = true
+-- vim.opt.wrap = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.relativenumber = true
@@ -15,7 +15,7 @@ vim.opt.colorcolumn = { "120" }
 lvim.log.level = "info"
 lvim.format_on_save = {
   enabled = true,
-  pattern = { "*.lua", "*.go", "*.py" },
+  pattern = { "*.lua", "*.go", "*.py", "*.css" },
   timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
@@ -132,6 +132,11 @@ formatters.setup {
     command = "black",
     args = { "--line-length", "120" },
   },
+  {
+    command = "prettier",
+    -- extra_args = { "--print-width", "100" },
+    filetypes = { "typescript", "typescriptreact", "css" },
+  },
 }
 
 local linters = require "lvim.lsp.null-ls.linters"
@@ -183,6 +188,8 @@ lvim.plugins = {
   -- themes
   { "Mofiqul/vscode.nvim" },
 
+  { "kevinhwang91/nvim-ufo", dependencies = { "kevinhwang91/promise-async" } },
+
   {
     "folke/todo-comments.nvim",
     event = "BufRead",
@@ -229,3 +236,31 @@ dapgo.setup {
     },
   },
 }
+
+
+
+-- folding config
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+-- Tell the server the capability of foldingRange,
+-- Neovim hasn't added foldingRange to default capabilities, users must add it manually
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true
+}
+local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+for _, ls in ipairs(language_servers) do
+  require('lspconfig')[ls].setup({
+    capabilities = capabilities
+    -- you can add other fields for setting up lsp server in this table
+  })
+end
+require('ufo').setup()
